8742462006d104f82550da15a4a163a02f4d9414
Simon Rose <simonrose121@gmail.com>
2020-10-08 14:54:25 +0100

    Update text and formating

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function updateYear() {
-    let id = document.getElementById('current-year');
-    const currentDate = new Date();
-    const currentYear = currentDate.getFullYear();
-    id.innerHTML = currentYear;
+	let id = document.getElementById("current-year");
+	const currentDate = new Date();
+	const currentYear = currentDate.getFullYear();
+	id.innerHTML = currentYear;
 }
 
 updateYear();
